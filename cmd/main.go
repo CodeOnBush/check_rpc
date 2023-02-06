7e65b8e6f75bfaa9b4a4225c3ffdc9050264bae2
PunkCoder <codeonbush@foxmail.com>
2023-02-06 15:55:26 +0800

    init

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+// Package check_rpc
+// @author chenfeng8@satff.weibo.com
+// @date 2023/2/6
+// @note
+///**
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	return
+}
